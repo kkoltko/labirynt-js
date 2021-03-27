f13717cfd0569db64179ab0f047437e3b5b5d546
kkoltko <kkoltko@gmail.com>
2021-03-27 13:05:31 +0100

    data

--- data.ts
+++ data.ts
@@ -1,1 +1,27 @@
-// Dodaj tutaj swój kod
+namespace data {
+    //% block
+    export function getData(): any {
+        return {
+            "name": "Moja krypta",
+            "levels": [
+                {
+                    "roomStart": 10,
+                    "roomEnd": 61,
+                    "rooms": [
+                        {
+                            "no": 11,
+                            "doors": "GP",
+                            "score": 1
+                        },
+                        {
+                            "no": 12,
+                            "doors": "LP",
+                            "score": 1
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+}
